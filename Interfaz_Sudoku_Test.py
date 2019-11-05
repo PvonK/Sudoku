076c5d4cd305a03c96e5c34385b6e70f94e914f7
PvonK <philippvonkesselstatt@gmail.com>
2019-11-05 13:02:42 -0300

    changed requirements.txt to add parameterized and removed magick mock from test setup

--- Interfaz_Sudoku_Test.py
+++ Interfaz_Sudoku_Test.py
@@ -3,25 +3,24 @@ import io
 from parameterized import parameterized
 from unittest.mock import patch, MagicMock
 from Interfaz_Sudoku import Interfaz
+from Sudoku import Sudoku
 
 
 class TestInterfazSudoku(unittest.TestCase):
 
     def setUp(self):
+
+        self.user4 = Interfaz()
+        self.user9 = Interfaz()
+
         lista4 = [["4", "x", "3", "1"],
                   ["x", "3", "x", "x"],
                   ["3", "1", "x", "2"],
                   ["x", "4", "x", "x"]]
-        self.user4 = Interfaz()
-        mock = MagicMock()
-        mock.side_effect = ["4", "1"]
-        with patch("builtins.input",
-                   new=mock
-                   ), patch(
-                   "Interfaz_Sudoku.api",
-                   return_value=lista4
-                   ):
-            self.user4.inicio()
+
+        self.user4.tam = 4
+        self.user4.level = "1"
+        self.user4.game = Sudoku(lista4)
 
         lista9 = [
                     ["5", "3", "x", "x", "7", "x", "x", "x", "x"],
@@ -34,16 +33,9 @@ class TestInterfazSudoku(unittest.TestCase):
                     ["x", "x", "x", "4", "1", "9", "x", "x", "5"],
                     ["x", "x", "x", "x", "8", "x", "x", "7", "9"]]
 
-        self.user9 = Interfaz()
-        mock = MagicMock()
-        mock.side_effect = ["9", "1"]
-        with patch("Interfaz_Sudoku.api",
-                   return_value=lista9
-                   ), patch(
-                   "builtins.input",
-                   new=mock
-                   ):
-            self.user9.inicio()
+        self.user9.tam = 9
+        self.user9.level = "1"
+        self.user9.game = Sudoku(lista9)
 
     @parameterized.expand([
         ("a", "4", "6"),
--- dockerfile
+++ dockerfile
@@ -4,6 +4,5 @@ RUN git clone https://github.com/PvonK/Sudoku
 WORKDIR /Sudoku
 
 RUN pip install -r requirements.txt
-RUN pip install parameterized
 
 CMD [ "python3", "all_tests.py" ] && [ "python3", "Interfaz_Sudoku.py" ]
